0de18a43e7e93c267e08ebfc602e595a86b7c0b4
Dave Shuck <dshuck@gmail.com>
2015-09-15 23:52:40 -0400

    Addition of groovy templating

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -1,13 +1,28 @@
+import ratpack.groovy.template.TextTemplateModule
+
+import static ratpack.groovy.Groovy.groovyTemplate
 import static ratpack.groovy.Groovy.ratpack
 
 ratpack {
+	bindings {
+		module new TextTemplateModule()
+	}
+
 	handlers {
+
+		def title = 'Our Ratpack Playground'
 		get {
 			render 'Hello, World!'
 		}
 
 		get(':name') {
-			render "Hello, $pathTokens.name!"
+			render( groovyTemplate( 'index.html', title: title, message: "Hello, $pathTokens.name!" ))
+		}
+
+		prefix( 'dummy') {
+			get(':name') {
+				render "Hello dummy, $pathTokens.name!"
+			}
 		}
 	}
 }
